b2ca2913b7b4c460ea73fa13d32e30a1623ed707
XFSZTechy <84674829+XFSZTechy@users.noreply.github.com>
2023-05-22 09:47:12 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,15 @@
+Citizen.CreateThread(function()
+
+    Citizen.Wait(5*1000)
+    
+    SetMapName('GTA5 MAP')
+    
+    end)
+
+    Citizen.CreateThread(function()
+    
+    Citizen.Wait(5*1000)
+    
+    SetGameType('roleplay')
+    
+    end)
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Diamond_RP'
+description 'set maptype and game type'
+version '2.0.0'
+
+client_scripts {
+    'client.lua'
+}
